af89ff0cdf5985c12bfcf58a26adf6dad96f0c30
volldammd <volldammd@gmail.com>
2016-06-20 11:36:55 -0400

    Initial Commit

---
+++ aVian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aVian
+//
+//  Created by Cerebro on 6/20/16.
+//  Copyright © 2016 volldammd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aVian/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aVian
+//
+//  Created by Cerebro on 6/20/16.
+//  Copyright © 2016 volldammd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
